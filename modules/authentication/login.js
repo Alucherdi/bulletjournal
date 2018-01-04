4f4614cc5303e357e3c6fa517732a2da3f3d2015
Alucherdi <hjgo127942@gmail.com>
2018-01-04 17:53:14 -0600

    adding div style integration to the task grid

--- modules/authentication/login.js
+++ modules/authentication/login.js
@@ -1,6 +1,8 @@
 var express = require("express")
 var app = module.exports = express()
 
+app.use(express.static(__dirname + "/public"))
+
 app.get("/login", (req, res) => {
     res.sendFile(__dirname + "/test.html")
 })
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express")
 var app = express()
 
+
 app.use(require("./modules/authentication/login"))
 
 app.listen(4200, () => console.log("Listening to 4200"))
